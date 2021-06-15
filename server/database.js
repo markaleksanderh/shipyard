57d1380b56fba2a4f53f1b91aff7920cac313741
Mark Hil <aleksanderhil@gmail.com>
2021-06-15 21:06:26 +0100

    Added db.js

---
+++ server/database.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_SCHEMA || 'postgres',
+                                process.env.DB_USER || 'postgres',
+                                process.env.DB_PASSWORD || '',
+                                {
+                                    host: process.env.DB_HOST || 'localhost',
+                                    port: process.env.DB_PORT || 5432,
+                                    dialect: 'postgres',
+                                    dialectOptions: {
+                                        ssl: process.env.DB_SSL == "true"
+                                    }
+                                });
+const Note = sequelize.define('Note', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+});
+module.exports = {
+    sequelize: sequelize,
+    Note: Note
+};
---
+++ server/routes/standup-note.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../database');
+
+router.get("/all", function(req, res) {
+    db.Note.findAll()
+        .then( notes => {
+            res.status(200).send(JSON.stringify(notes));
+        })
+        .catch( err => {
+            res.status(500).send(JSON.stringify(err));
+        });
+});
+
+router.get("/:id", function(req, res) {
+    db.Note.findByPk(req.params.id)
+        .then( note => {
+            res.status(200).send(JSON.stringify(note));
+        })
+        .catch( err => {
+            res.status(500).send(JSON.stringify(err));
+        });
+});
+
+router.put("/", function(req, res) {
+    db.Note.create({
+        title: req.body.title,
+        description: req.body.description,
+        id: req.body.id
+        })
+        .then( note => {
+            res.status(200).send(JSON.stringify(note));
+        })
+        .catch( err => {
+            res.status(500).send(JSON.stringify(err));
+        });
+});
+
+router.delete("/:id", function(req, res) {
+    db.Note.destroy({
+        where: {
+            id: req.params.id
+        }
+        })
+        .then( () => {
+            res.status(200).send();
+        })
+        .catch( err => {
+            res.status(500).send(JSON.stringify(err));
+        });
+});
+
+module.exports = router;
