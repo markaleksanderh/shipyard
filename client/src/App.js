53dbb0cd6478085c63a45276a120dffdff91d9b5
Mark Hil <aleksanderhil@gmail.com>
2021-06-22 23:38:58 +0100

    feat: working jwt auth on BE

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
 
-import Header from './components/Header/Header'
+// import Header from './components/Header/Header'
 // import ProjectForm from './components/ProjectForm/ProjectForm'
-import NoteList from './components/NoteList/NoteList'
+// import NoteList from './components/NoteList/NoteList'
 // import NoteForm from './components/NoteForm/NoteForm'
-import CompanyForm from './components/CompanyForm/CompanyForm'
-import CompanyList from './components/CompanyList/CompanyList'
-import ProjectList from './components/ProjectList/ProjectList'
+// import CompanyForm from './components/CompanyForm/CompanyForm'
+// import CompanyList from './components/CompanyList/CompanyList'
+// import ProjectList from './components/ProjectList/ProjectList'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <h2>Companies</h2>
-        <CompanyForm />
-        <CompanyList />
-        <h2>Projects</h2>
-        <ProjectList />
-        <h2>Notes</h2>
-        <NoteList />
-      </div>
+      <h1>React/Express/Postgres/Docker</h1>
+      // <div className="App">
+      //   <Header />
+      //   <h2>Companies</h2>
+      //   <CompanyForm />
+      //   <CompanyList />
+      //   <h2>Projects</h2>
+      //   <ProjectList />
+      //   <h2>Notes</h2>
+      //   <NoteList />
+      // </div>
 );
   }
 }
