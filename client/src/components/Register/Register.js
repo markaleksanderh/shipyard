d9ad1e57458c494357c82ced0dbe0008201e791c
Mark Hil <mark@100shapes.com>
2021-06-27 18:02:30 +0100

    Redirect user to home after register

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -3,6 +3,7 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 // import AuthService from "../../services/auth.service";
 import Alert from 'react-bootstrap/Alert'
+import authService from "../../services/auth.service";
 
 
 
@@ -67,7 +68,15 @@ export default class Register extends Component {
       return false
     }
     else {
-      console.log('Submit form')
+      authService.register(
+        this.state.username,
+        this.state.email,
+        this.state.password
+      ).then(()=> {
+        this.props.history.push('');
+        window.location.reload();
+      })
+      console.log("Success")
     }
   }
 
@@ -114,122 +123,3 @@ export default class Register extends Component {
     )
   }
 }
-
-
-// export default class Register extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.handleRegister = this.handleRegister.bind(this);
-    // this.onChangeUsername = this.onChangeUsername.bind(this);
-    // this.onChangeEmail = this.onChangeEmail.bind(this);
-    // this.onChangePassword = this.onChangePassword.bind(this);
-//     this.state = {
-//       username: "",
-//       email: "",
-//       password: "",
-//       successful: false,
-//       message: ""
-//     };
-//   }
-
-  // onChangeUsername(event) {
-  //   this.setState({
-  //     username: event.target.value
-  //   });
-  // }
-
-  // onChangeEmail(event) {
-  //   this.setState({
-  //     email: event.target.value
-  //   });
-  // }
-
-  // onChangePassword(event) {
-  //   this.setState({
-  //     password: event.target.value
-  //   });
-  // }
-
-//   handleRegister(event) {
-//     event.preventDefault();
-
-//     this.setState({
-//       message: "",
-//       successful: false
-//     });
-
-
-//     AuthService.register(
-//       this.state.username,
-//       this.state.email,
-//       this.state.password
-//     ).then( res => {
-//       this.setState({
-//         message: res.data.message,
-//         successful: true
-//       })
-//     },
-//     err => {
-//       this.setState({
-//         successful: false,
-//         message: err.toString()
-//       })
-//     }
-//     )
-//   }
-
-//   render() {
-//     return (
-//           <Form onSubmit={this.handleRegister} ref={c => {this.form = c;}}>
-//             {!this.state.successful && (
-//               <div>
-                // <Form.Group controlId="formBasicUsername">
-                //   <Form.Label>Username</Form.Label>
-                //   <Form.Control 
-                //   type="username" 
-                //   placeholder="Enter username" 
-                //   name="username" 
-                //   value={this.state.username} 
-                //   onChange={this.onChangeUsername}
-                //   validations={[required, vusername]}
-                //   />
-                // </Form.Group>
-                // <Form.Group controlId="formBasicEmail">
-                //   <Form.Label>Email address</Form.Label>
-                //   <Form.Control
-                //   type="email"
-                //   placeholder="Enter email"
-                //   name="email"
-                //   value={this.state.email}
-                //   onChange={this.onChangeEmail}
-                //   validations={[required, email]}
-                //   />
-                // </Form.Group>
-                // <Form.Group controlId="formBasicPassword">
-                //   <Form.Label>Password</Form.Label>
-                //   <Form.Control 
-                //   type="password" 
-                //   placeholder="Password" 
-                //   name="password" 
-                //   value={this.state.password} 
-                //   onChange={this.onChangePassword}
-                //   validations={[required, vpassword]}
-                //   />
-                // </Form.Group>
-                // <Button variant="primary" type="submit">
-                //   Submit
-                // </Button>
-//               </div>
-//             )}
-//             {this.state.message && 
-//               (
-//                 this.state.successful ? <Alert variant='success'>this.state.message</Alert> : <Alert variant='danger'>this.state.message</Alert>
-//               )
-//             }
-//             {/* <VCheckButton style={{ display: "none" }} ref={c => { this.checkBtn = c; }} /> */}
-//           </Form>
-//     );
-//   }
-// }
-
-
